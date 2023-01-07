88f9fba387e89b5b5a992afc95767a604625baac
denervalspelizario <denervalspelizario@gmail.com>
2023-01-07 13:25:50 -0300

    estudo react-native estilizacao inline e importacao de imagem

--- meuApp/App.js
+++ meuApp/App.js
@@ -1,12 +1,22 @@
 import React, {Component} from "react";  // o component é para poder ter classes
-import {View, Text} from 'react-native';  // a View é igual a div - Text é tipo o h1 ou o p
+import {View, Text, Image} from 'react-native';  // a View é igual a div - Text é tipo o h1 ou o p - Image é para importar imagens
  
 class App extends Component {  // maneira diferente de criar component diferente do comum - export default function App()
   render(){
     return(
-      <View>
-        <Text>Desenvolvimento Mobile</Text>
+      <View> 
+        <Text>Desenvolvimento Mobile</Text> 
         <Text>React-Native</Text>
+        {/* estilizando in line nao recomendado */}
+        <Text style={{ color: '#FF0000', fontSize: 25, margin: 15 }}>
+          Por D S Pelizario 
+        </Text>
+
+        <Image 
+          source={{uri: 'https://sujeitoprogramador.com/steve.png'}}
+          style={{width: 300, height: 300, marginLeft: 50}}
+        />
+
       </View>
     )
   }
