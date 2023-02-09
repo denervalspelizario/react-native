cb7ebae4e2c14aa53b6ac415d4d24a08c0576a31
denervalspelizario <denervalspelizario@gmail.com>
2023-02-09 12:25:31 -0300

    estudo react-native importacao e uso de modal

---
+++ 14_Modal/App.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button, Modal  } from 'react-native';  // importando botao e modal 
+import Entrar from './component/Entrar';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      modalVisible: false // state para uso do modal 
+    }
+    this.entrar = this.entrar.bind(this); // bind para linkar conteudo da state(usar conteudo da state) a funcao entrar 
+    this.sair = this.sair.bind(this); // bind para linkar conteudo da state(usar conteudo da state) a funcao sair 
+  }
+
+  entrar(){ // funcao entrar chamada ao clicar no botao(linha 22)
+    if(this.state.modalVisible == false){  
+      this.setState({modalVisible: true}) //  altera o state modalVisible para true e assim mostra o modal(olhar linha 29)
+    }
+  }
+
+  sair(){ // funcao sair chamada ao clicar no botao(linha 13 Entrar.js)
+    if(this.state.modalVisible == true){  
+      this.setState({modalVisible: false}) //  altera o state modalVisible para false e assim mostra o modal(olhar linha 29)
+    }
+  }
+
+  render(){
+  return (
+    <View style={styles.container}>
+       {/* Botão  com nome Entrar ao clicar(onPress) chama funcao entrar */}   
+      <Button title='Entrar' onPress={ this.entrar}/>
+
+
+      <Modal 
+        transparent={true} // deixa o fundo fora do modal transparent
+        animationType='slide' // modal inicia tela de baixo pra cima existe a opçao 'fade' também ela aparece na tela sem animacao (olhar documentacao)
+                              // https://reactnative.dev/docs/modal                                
+        visible={this.state.modalVisible} // mostra quando sera MOSTRADA o modal, neste caso quando o state modalVisible estiver true 
+      >
+        {/* Recebe o componente entrar   que recebe o props(parametro) sair que é uma funcao (linha 22)*/}
+        <Entrar sair={this.sair}/>  
+      </Modal>
+    </View>
+  );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DDD',
+  },
+  textModal: {
+    color: '#fff',
+    fontSize: 28,
+  },
+  
+  
+});
+
+
+export default App;
---
+++ 14_Modal/component/Entrar.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,  } from 'react-native'; 
+
+class Entrar extends Component {
+
+  render(){
+  return (
+    
+        <View style={styles.containerModal}>
+          <Text style={styles.textModal}>Seja bem vindo!!</Text>
+          
+                                        {/* Ao clicar(onPress) chama o props no caso a funcao sair (linha 22 app.js)*/}
+          <TouchableOpacity style={styles.btn}  onPress={this.props.sair}>
+            <Text style={styles.textBtn}>Sair</Text>
+          </TouchableOpacity>
+        </View>
+     
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+    containerModal: {
+      width: '100%',
+      height: 300,
+      alignItems: 'center',
+      backgroundColor: '#292929',
+      marginTop: '50%',
+    },
+    textModal: {
+      marginTop: '20%',  
+      color: '#fff',
+      fontSize: 28,
+    },
+    btn: {
+      width: 70,
+      height: 50,
+      borderRadius: 20,  
+      marginTop: 80,   
+      backgroundColor: '#6A5ACD',  
+      justifyContent: 'center',
+      alignItems: 'center',  
+    },
+    textBtn:{
+      fontSize: 20,
+      color: '#FFF'          
+    }
+    
+    
+  });
+
+
+export default Entrar;
