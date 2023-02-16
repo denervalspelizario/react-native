3ce4b542715af60361f0dcd8ca2973137e8bd949
denervalspelizario <denervalspelizario@gmail.com>
2023-02-16 09:34:04 -0300

    estudo react-native animcao sequence e parallel

--- 16_Animacao/App.js
+++ 16_Animacao/App.js
@@ -6,23 +6,65 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state= {
-      larguraAnimada: new Animated.Value(150), // state animada com valor inicial 150
-      alturaAnimada: new Animated.Value(50), // state animada com valor inicial 50
+      larguraAnimada: new Animated.Value(150), // state animada com valor inicial 150 (leia linha 65)
+      alturaAnimada: new Animated.Value(50), // state animada com valor inicial 50  (leia linha 66)
+      opacidadeAnimada: new Animated.Value(0)
 
     };
 
-    Animated.timing(
-      this.state.larguraAnimada,  
-      {                             // parametros de animação da larguraAnimada
+    Animated.sequence([ /* quando precisa de uma sequancia de varia animações  o animated.timing fica dentro de um animated.sequence
+                           ** o sequence indica animacao em sequencia se quisermos que todas as animações se façam juntas 
+                           alterasse sequence para parallel  olhar documentacao https://reactnative.dev/docs/animated */
+
+      Animated.timing(  // a cada animação pedida a estrutura é a mesma => Animated.timing => state que deseja alterar => alteração 
+        this.state.opacidadeAnimada,
+        {
+          toValue: 1,
+          duration: 3000
+        }           
+      ),   
+      
+      Animated.parallel([   // animação começa com sequence(em cascata ou sequnciada) e agora animação são feitas em paralelo(tudo junto)
+
+        Animated.timing( // a cada animação pedida a estrutura é a mesma => Animated.timing => state que deseja alterar => alteração 
+          this.state.larguraAnimada,
+          {
+            toValue: 300,
+            duration: 2000
+          }
+        ),
+        Animated.timing(  // a cada animação pedida a estrutura é a mesma => Animated.timing => state que deseja alterar => alteração 
+          this.state.alturaAnimada,
+          {
+            toValue: 300,
+            duration: 2000
+          }
+        ),
+      ]),
+      // apartir daqui volta a ser em sequence(cascata ou sequencia)
+      Animated.timing(  // a cada animação pedida a estrutura é a mesma => Animated.timing => state que deseja alterar => alteração 
+        this.state.opacidadeAnimada,
+        {
+          toValue: 0,
+          duration: 3000
+        }           
+      ),  
+
+    ]).start()  // funcao para estartar a  animação lembrando que a animacao neste caso esta fazendo em cascata faz a do state alrguraAnimada e depois alturaAnimada
+
+
+
+   /* Animated.timing( 
+      this.state.larguraAnimada,  // parametros de animação da state larguraAnimada
+      {                             
         toValue: 300, // altera de 150 para 300 o valor(leia linha 9)
         duration: 2000  // durantion em milissegundos ou seja altera o valor de 150 pra 300 em 2 segundos
       },
       
 
-    ).start() // funcao para estartar a animação
+    ).start() // funcao para estartar a animação */
 
   }
-
 
   render(){
   return (
@@ -31,6 +73,7 @@ class App extends Component {
       <Animated.View style={{  // estamos dizendo que a View será animada
                     width: this.state.larguraAnimada, // o tamanho será uma state que será alterada
                     height: this.state.alturaAnimada, // a altura será uma state que será alterada
+                    opacity: this.state.opacidadeAnimada, // a opacidade será uma state que será alterada
                     backgroundColor: '#4169e1', 
                     justifyContent: 'center'
                   }}>
